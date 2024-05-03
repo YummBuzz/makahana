40d0cb172515040ecabcbfcbef227573ae7c1545
Jitender090 <jitender.96@yahoo.com>
2024-05-03 14:15:04 +0530

    wip

--- frontend/src/components/user/auth/Login.jsx
+++ frontend/src/components/user/auth/Login.jsx
@@ -1,19 +1,18 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
-import Button from 'react-bootstrap/Button';
-import axios from 'axios'
-
-
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+// import {useQuery} from '@tanstack/react-query'
 
 export default function Login() {
-    const [loggedIn, setLoggedIn] = useState(false);
-const [loading, setLoading] = useState(false);
-const [popup,setPopup]=useState(false);
-const [result,setResult]=useState("")
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [popup, setPopup] = useState(false);
+  const [result, setResult] = useState("");
 
-const [formData, setFormData] = useState({
-    username: '',
-    password: '',
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
   });
 
   const handleChange = (e) => {
@@ -24,27 +23,50 @@ const [formData, setFormData] = useState({
     });
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     e.preventDefault();
-    setLoading(true); 
-    setPopup(true)
+    setLoading(true);
+    setPopup(true);
     try {
-      
-      const response = await axios.post('http://localhost:3800/loginAuthenticate', formData);
+      const response = await axios.post(
+        "http://localhost:3800/loginAuthenticate",
+        formData
+      );
       console.log(response.data);
-      localStorage.setItem('token', response.data.token);
+      localStorage.setItem("token", response.data.token);
       setLoggedIn(true);
-      setResult(response.data.message)
-      console.log('Form submitted with data:', formData);
+      setResult(response.data.message);
+      console.log("Form submitted with data:", formData);
     } catch (error) {
-      console.error('An error occurred:', error);
-      setResult(error.response.data)
-    //   console.log(error.response.data)
+      console.error("An error occurred:", error);
+      setResult(error.response.data);
+      //   console.log(error.response.data)
     } finally {
       setLoading(false); // Set loading state back to false after API call completes
     }
   };
+
+//   const fetchData = async () => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//     const resp = await axios.get("http://localhost:3800/profile", {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     return resp.data;
+//   };
+// }
+
+//  const {data,isLoading,isError}=useQuery({
+//     queryKey:["profile"],
+//     queryFn:fetchData
+    
+//   })
+//   console.log(data)
+  // console.log(isLoading)
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     console.log(token)
@@ -54,12 +76,12 @@ const [formData, setFormData] = useState({
           'Authorization': `Bearer ${token}`
         }
       })
-     
+
       .then((response) => {
-       
-        console.log(response.data); 
-        
-        setLoggedIn(true); 
+
+        console.log(response.data);
+
+        setLoggedIn(true);
       })
         .catch((error) => {
           if (error.response && error.response.status === 401) {
@@ -73,45 +95,41 @@ const [formData, setFormData] = useState({
     }
   });
 
-
   return (
     <>
       <div className="main">
         <div>Login</div>
-       
-       {popup && (
+
+        {popup && (
           <div className="popup">
             <div className="sub-popup">
               <p>{result}</p>
               <button onClick={() => setPopup(false)}>Logout</button>
-             
-             
             </div>
           </div>
         )}
 
-
-
- 
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="username"
-        value={formData.username}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-      {/* <Button type="submit" disabled={!formData.username || !formData.password}>Submit</Button> */}
-      <Button type="submit" disabled={loading || !formData.username || !formData.password}>
-        {loading ? 'Submitting...' : 'Submit'}
-      </Button>
-    </form>
-
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {/* <Button type="submit" disabled={!formData.username || !formData.password}>Submit</Button> */}
+          <Button
+            type="submit"
+            disabled={loading || !formData.username || !formData.password}
+          >
+            {loading ? "Submitting..." : "Submit"}
+          </Button>
+        </form>
       </div>
     </>
   );
