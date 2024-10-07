d0852dd21e120f458a5758e61799f225267806a4
YummBuzz <yummbuzzonline@gmail.com>
2024-10-07 17:45:12 +0530

    wip

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require( 'mongoose' );
 async  function database(){
    
     // await mongoose.connect("mongodb://0.0.0.0:27017/makhana");
-    await mongoose.connect("mongodb+srv://jitenderbachhraj5:oAuT02EGYWv6pDjn@poultry.uq1ykby.mongodb.net/makana?retryWrites=true&w=majority&appName=makana");
+    await mongoose.connect("mongodb+srv://yummbuzzonline:nXGNyIX3SBR0Ufxu@poultry.uq1ykby.mongodb.net/makana?retryWrites=true&w=majority&appName=makana");
     console.log("Database Connected");
 
 }
