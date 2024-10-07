72902561f77666a205c9158cca94256361f9a038
YummBuzz <yummbuzzonline@gmail.com>
2024-10-07 17:30:02 +0530

    wip

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ var app=express()
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://makhana-rose.vercel.app"],
+        origin: [`${process.env.URL}`],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
